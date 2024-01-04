c861649754a2e18752fc3c13c46ed4b902bea7ad
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-01-04 16:37:19 +0200

    playwright.config fixed

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,10 @@ module.exports = {
   reporter: process.env.CI
     ? [['dot'], ['html', { outputFolder: 'playwright-report' }]] // for CI: use concise 'dot' and 'html' reporters
     : [['list'], ['html', { outputFolder: 'playwright-report' }]], // fol local run: use default 'list' and 'html' reporters
-  workers: 5
-    // process.env.CI
-    // ? 5 // for CI: run up to 5 tests concurrently              process.env.CI_WORKERS ||
-    // : 1, // for local run: run up to 2 tests concurrently
+  workers: 5,
+  // process.env.CI
+  // ? 5 // for CI: run up to 5 tests concurrently              process.env.CI_WORKERS ||
+  // : 1, // for local run: run up to 2 tests concurrently
   // timeout: 0, // Unlimited Timeout for debugging
   timeout: 60000, // Timeout of 60 seconds
   retries: 2, // Defines the maximum attempts to retry a test after a failure
