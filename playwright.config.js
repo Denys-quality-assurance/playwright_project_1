1d81bdab6c5c1c9ddde28ea8f7f749f66768285e
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-01-04 16:26:20 +0200

    playwright.config updated: for CI: run up to 5 tests concurrently

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ module.exports = {
     ? [['dot'], ['html', { outputFolder: 'playwright-report' }]] // for CI: use concise 'dot' and 'html' reporters
     : [['list'], ['html', { outputFolder: 'playwright-report' }]], // fol local run: use default 'list' and 'html' reporters
   workers: process.env.CI
-    ? process.env.CI_WORKERS || 5 // for CI: run up to 5 tests concurrently
+    ? 5 // for CI: run up to 5 tests concurrently              process.env.CI_WORKERS ||
     : 1, // for local run: run up to 2 tests concurrently
   // timeout: 0, // Unlimited Timeout for debugging
   timeout: 60000, // Timeout of 60 seconds
