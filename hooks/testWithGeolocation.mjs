b6694953de410dcd32b0afc850462f7598c83182
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-01-09 17:15:46 +0200

    createSharedContextTest() implemented

--- hooks/testWithGeolocation.mjs
+++ hooks/testWithGeolocation.mjs
@@ -1,26 +1,10 @@
-import { test as base } from '@playwright/test';
+import { createSharedContextTest } from './baseWithSharedContext.mjs';
 
-const test = base.extend({
-  sharedContext: async ({ browser }, use) => {
-    const context = await browser.newContext({
-      permissions: [`geolocation`], // Allow Google to track the geolocation
-      ignoreHTTPSErrors: true,
-    });
-    await use(context);
-    await context.close();
-  },
-});
+const contextOptions = {
+  permissions: [`geolocation`], // Allow Google to track the geolocation
+  ignoreHTTPSErrors: true,
+};
 
-test.afterEach({ timeout: 120000 }, async ({ sharedContext }, testInfo) => {
-  const pages = sharedContext.pages(); // get all open pages
-  for (let i = 0; i < pages.length; i++) {
-    // Add viewport screenshots as attachments to HTML report
-    const screenshotViewport = await pages[i].screenshot();
-    // const screenshotFullPage = await pages[i].screenshot({ fullPage: true }); // Add fullPage screenshots for debugging
-
-    await testInfo.attach(`viewport_screenshot_of_Page_${i}`, { body: screenshotViewport, contentType: 'image/png' });
-    // await testInfo.attach(`fullpage_screenshot_of_Page_${i}`, { body: screenshotFullPage, contentType: 'image/png' }); // Add fullPage screenshots for debugging
-  }
-});
+const test = createSharedContextTest(contextOptions);
 
 export default test;
