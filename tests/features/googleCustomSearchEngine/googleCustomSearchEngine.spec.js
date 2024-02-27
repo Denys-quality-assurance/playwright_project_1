6064569827d1c52031299db62a9df42c6ace0f62
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-02-28 01:00:45 +0200

    tests/features filder sctucture updated, specs devided by folders

--- tests/features/googleCustomSearchEngine/googleCustomSearchEngine.spec.js
+++ tests/features/googleCustomSearchEngine/googleCustomSearchEngine.spec.js
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
-import test from '../../hooks/testWithAfterEachHooks.mjs';
-import GoogleCustomSearchEnginePage from '../pages/googleCustomSearchEngineIframe';
-import { queryDataGeneral } from '../test-data/queryData';
+import test from '../../../hooks/testWithAfterEachHooks.mjs';
+import GoogleCustomSearchEnginePage from '../../pages/googleCustomSearchEngineIframe';
+import { queryDataGeneral } from '../../test-data/googleSearch/queryData';
 const query = queryDataGeneral[0].query;
 
 test.describe(`Google Custom Search Engine: Search results testing for '${query}' query`, () => {
