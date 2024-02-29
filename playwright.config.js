a70bde436ebd9876c5454b765e476afa13d53df8
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-02-29 23:50:50 +0200

    playwright.config updated: testMatch: 'tests/**/*.spec.js'

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,7 @@ module.exports = {
   workers: process.env.CI ? 4 : 2, // For CI: run up to 4 tests concurrently; for local run: run up to 2 tests concurrently
   timeout: 30000, // Timeout of 30 seconds: 30000. Unlimited Timeout for debugging - timeout: 0
   retries: 2, // Defines the maximum attempts to retry a test after a failure
+  testMatch: 'tests/**/*.spec.js', // Glob patterns or regular expressions that match test files
   projects: [
     /* Test against desktop browsers */
     {
