13e13d438b0a6d6f6724322c0783826a1b909a11
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-03-06 18:31:04 +0200

    vscode settings added

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  arrowParens: 'always',
-  bracketSpacing: true,
-  embeddedLanguageFormatting: 'auto',
-  endOfLine: 'lf',
-  htmlWhitespaceSensitivity: 'css',
-  printWidth: 80,
-  proseWrap: 'preserve',
-  quoteProps: 'as-needed',
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
+  arrowParens: 'always', // Include parens around a single arrow function parameter
+  bracketSpacing: true, // Print spaces between brackets in object literals
+  embeddedLanguageFormatting: 'auto', // Controls how Prettier formats embedded code
+  endOfLine: 'lf', // Enforce use of Linux end of line
+  htmlWhitespaceSensitivity: 'css', // Adjusts how HTML whitespace is handled
+  printWidth: 80, // Specify the line length that the printer will wrap on
+  proseWrap: 'preserve', // Controls how prose blocks should be wrapped in markdown
+  quoteProps: 'as-needed', // Change when properties in objects are quoted
+  semi: true, // Print semicolons at the ends of statements
+  singleQuote: true, // Use single quotes instead of double quotes
+  tabWidth: 2, // Specify the number of spaces per indentation-level
+  trailingComma: 'es5', // Trailing commas where valid in ES5: in arrays and objects
 };
