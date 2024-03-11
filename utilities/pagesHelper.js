a0b2f58ed9db903b22281f632aad176555980d39
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-03-11 22:15:00 +0200

    pagesHelper updated: comments added; navigateHomeForNewPage reused in performSearchAndFetchResultsForNewPage

--- utilities/pagesHelper.js
+++ utilities/pagesHelper.js
@@ -1,3 +1,10 @@
+/*
+ * This file provides utilities for creating a new page in the same context.
+ * This is particularly useful when conducting searches on multiple
+ * pages at the same time within the same browsing context.
+ *
+ */
+
 // Create new page in the same context, navigate to Home page and reject all Cookies if it's needed
 export async function navigateHomeForNewPage(
   sharedContext,
@@ -6,6 +13,7 @@ export async function navigateHomeForNewPage(
   const newPage = await sharedContext.newPage();
   const isMobile = sharedContext._options.isMobile || false; // type of device is mobile
   const googleSearchPage = new GoogleSearchPageClass(newPage, isMobile);
+  // Navigate to the home page and reject all cookies
   await googleSearchPage.navigateAndRejectCookies();
   return { newPage, googleSearchPage };
 }
@@ -17,19 +25,23 @@ export async function performSearchAndFetchResultsForNewPage(
   GoogleSearchPageClass,
   searchFunction
 ) {
-  const newPage = await sharedContext.newPage();
-  const isMobile = sharedContext._options.isMobile || false; // type of device is mobile
-  const googleSearchPage = new GoogleSearchPageClass(newPage, isMobile);
-  await googleSearchPage.navigateAndRejectCookies();
+  // Create new page in the same context, navigate to Home page and reject all Cookies if it's needed
+  const { googleSearchPage } = await navigateHomeForNewPage(
+    sharedContext,
+    GoogleSearchPageClass
+  );
 
   // Perform search using the passed function
+  // If a custom search function is provided, use it to conduct the search. Otherwise, perform a basic search
   if (searchFunction) {
     await searchFunction(googleSearchPage, query);
   } else {
     await googleSearchPage.searchForQueryByEnter(query);
   }
 
+  // Get the locator for the search results
   const searchResultsLocator = await googleSearchPage.getSearchResultsLocator();
+  // Get all of the text content from the search results
   const searchResultsTexts =
     await googleSearchPage.getTextContent(searchResultsLocator);
 
