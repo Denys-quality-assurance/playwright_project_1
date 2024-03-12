e67db33a7b9abe15b2b64a92c8c0db86fd344b39
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-03-12 19:33:16 +0200

    acceptablePerformanceData updated: comments added

--- tests/test-data/googleSearch/acceptablePerformanceData.js
+++ tests/test-data/googleSearch/acceptablePerformanceData.js
@@ -1,3 +1,22 @@
+/*
+ * This module exports a constant object, acceptablePerformanceData, that represents a set
+ * of boundaries for the performance characteristics of a system under testing.
+ *
+ * The main purpose of maintaining this separate config is to keep track of acceptable
+ * performance metrics which can be updated in a centralized place according to
+ * changing performance standards or system modifications.
+ * Moreover, these define the 'pass/fail' criteria for performance tests.
+ *
+ * It currently only contains a property for search duration,
+ * but it is a placeholder for additional performance characteristics as the testing demands evolve.
+ *
+ * Other acceptable values of performance characteristics you might include are:
+ * - "acceptablePageLoadDuration": Maximum time allowed for a page to fully load.
+ * - "acceptableResponseTime": Maximum time allowed for the system to respond to a specific query.
+ * - "acceptableFirstPaint": Maximum time allowed until the browser renders the first pixels of the page.
+ *
+ */
+
 const acceptablePerformanceData = {
   acceptableSearchDutation: 5000, // Search results must be available to the user no later than this limit (ms)
 };
