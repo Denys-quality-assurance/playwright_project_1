ffda96cd483bd5ee9d8da1b0aa5c08e845c2b5eb
Denys-quality-assurance <Denys.Matolikov@burberry.com>
2024-03-15 01:46:07 +0200

    Comments to test modifications added

--- hooks/testWithAfterEachHooks.mjs
+++ hooks/testWithAfterEachHooks.mjs
@@ -1,3 +1,11 @@
+/*
+ * This module exports a `test` object which is used to create test cases.
+ * It uses the shared context created by 'createSharedContextTest' from './baseWithSharedContext.mjs'
+ *
+ * The shared context ensures that all tests are running under the same preconditions.
+ *
+ */
+
 import { createSharedContextTest } from './baseWithSharedContext.mjs';
 
 const test = createSharedContextTest();
--- hooks/testWithGeolocation.mjs
+++ hooks/testWithGeolocation.mjs
@@ -1,3 +1,19 @@
+/*
+ * This module exports a `test` object which is used to create test cases.
+ * It uses the shared context created by 'createSharedContextTest' from './baseWithSharedContext.mjs'
+ * with the context options.
+ *
+ * The context options defined here allow:
+ * - Google to track geolocation even if no explicit permission
+ *   has been granted for it.
+ * - Ignore HTTPS errors. Any error messages resulting from HTTPS requests
+ *   are ignored making it less verbose and suitable for production environment.
+ *
+ * The shared context ensures that all tests are running under the same preconditions
+ * across all test cases using `contextOptions`.
+ *
+ */
+
 import { createSharedContextTest } from './baseWithSharedContext.mjs';
 
 const contextOptions = {
